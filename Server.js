a5d80e1f2d1a36dd94a3dba0593f2506f9bdf53a
Farid Benbadis <farid.benbadis@lip6.fr>
2015-10-27 17:41:28 +0100

    ça marche pour l'instant. Faut rajouter handlebars

--- Server.js
+++ Server.js
@@ -8,6 +8,7 @@ var router = express.Router();
 var path = __dirname + '/views/';
 var token = "fcbe032df90e37d22e31ae006d4f71c5b92f2d96"
 var device_id = "54ff6b066672524852431867"
+var bodyParser = require('body-parser');
 
 var callback = function(err, body) {
   console.log('API call login completed on callback:', body);
@@ -15,7 +16,8 @@ var callback = function(err, body) {
 
 
 app.use(express.static('public'));
-url = 
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 router.use(function (req,res,next) {
   console.log("/" + req.method);
   next();
@@ -45,30 +47,25 @@ router.get("/chauffage",function(req,res){
   });
 });
 
-router.post("/chauffage",function(req,res){
-  console.log("Changement état de tous les chauffages")
-  //url = 'https://api.spark.io/v1/devices/'+device_id+'/fp?access_token='+token
-  //request(url, function (error, response, body) {
-  //  if (!error && response.statusCode == 200) {
-  //    reponse = JSON.parse(body);
-  //    console.log(reponse.result);
-  //    res.send(reponse.result)
-  //  }
-  //});
 
+router.post("/chauffage",function(req,res){
+  console.log("Changement état de tous les chauffages");
+  console.log(req.body.params);
+ 
   request({
     url: 'https://api.spark.io/v1/devices/'+device_id+'/fp?access_token='+token,
-    qs: {from: 'blog example', time: +new Date()}, //Query string data
+    //qs: {from: 'blog example', time: +new Date()}, //Query string data
     method: 'POST',
     //Lets post the following key/values as form
     json: {
       //access_token: token,
-      params: 'AAAAEAA'
+      params: req.body.params
     }}, function(error, response, body){
       if(error) {
         console.log(error);
       } else {
         console.log(response.statusCode, body);
+        res.sendStatus(response.statusCode)
       }
     });
 });
@@ -83,26 +80,3 @@ app.use("*",function(req,res){
 app.listen(3000,function(){
   console.log("Live at Port 3000");
 });
-//spark.login({accessToken: token}, callback);
-
-//spark.claimCore(device_id, function(err, data) {
-//  console.log('spark.claimCore err:', err);
-//  console.log('spark.claimCore data:', data);
-//});
-
-//spark.listDevices(function(err, devices) {
-//  var device = devices[0];
-//  console.log(device)
-//
-//  console.log('Device name: ' + device.name);
-//  console.log('- connected?: ' + device.connected);
-//  console.log('- variables: ' + device.variables);
-//  console.log('- functions: ' + device.functions);
-//  console.log('- version: ' + device.version);
-//  console.log('- requires upgrade?: ' + device.requiresUpgrade);
-//});
-
-//spark.getDevice(device_id, function(err, device) {
-//  console.log('Device name: ' + device.name);
-//});
-
