0eb169b4ca7b0c08d06f8153b859c849eae9bf9f
Farid <farid@ooii.io>
2015-10-28 11:26:30 +0100

    tout marche, parfait

--- Server.js
+++ Server.js
@@ -26,40 +26,31 @@ router.use(function (req,res,next) {
   console.log("/" + req.method);
   next();
 });
-router.get('/', function(req, res){
-  res.render('index');
-});
-app.get('/loop', function(req, res){
-  var basketballPlayers = [
-    {name: 'Lebron James', team: 'the Heat'},
-    {name: 'Kevin Durant', team: 'the Thunder'},
-    {name: 'Kobe Jordan',  team: 'the Lakers'}
-  ];
-  
-  var days = [
-    'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
-  ];
-  
-  var data = {
-    basketballPlayers: basketballPlayers,
-    days: days
-  };
-  
-  res.render('loop', data);
-});
+//router.get('/', function(req, res){
+//  res.render('index');
+//});
+//app.get('/loop', function(req, res){
+//  var basketballPlayers = [
+//    {name: 'Lebron James', team: 'the Heat'},
+//    {name: 'Kevin Durant', team: 'the Thunder'},
+//    {name: 'Kobe Jordan',  team: 'the Lakers'}
+//  ];
+//  
+//  var days = [
+//    'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
+//  ];
+//  
+//  var data = {
+//    basketballPlayers: basketballPlayers,
+//    days: days
+//  };
+//  
+//  res.render('loop', data);
+//});
 //router.get("/",function(req,res){
 //  res.sendFile(path + "index.html");
 //});
 
-var chauffages = [
-    {arret: false, eco: false, confort:false, horsgel:false, name: 'Entrée', id: '1', etat: ''},
-    {arret: false, eco: false, confort:false, horsgel:false, name: 'Emma', id: '2', etat: ''},
-    {arret: false, eco: false, confort:false, horsgel:false, name: 'Cuisine', id: '3', etat: ''},
-    {arret: false, eco: false, confort:false, horsgel:false, name: 'Milieu', id: '4', etat: ''},
-    {arret: false, eco: false, confort:false, horsgel:false, name: 'Salon', id: '5', etat: ''},
-    {arret: false, eco: false, confort:false, horsgel:false, name: 'SDB', id: '6', etat: ''},
-    {arret: false, eco: false, confort:false, horsgel:false, name: 'Douche', id: '7', etat: ''}
-  ];
 
 router.get("/about",function(req,res){
   res.sendFile(path + "about.html");
@@ -69,63 +60,84 @@ router.get("/contact",function(req,res){
   res.sendFile(path + "contact.html");
 });
 
-router.get("/chauffage",function(req,res){
+router.get("/",function(req,res){
+  var chauffages = [
+    {arret: false, eco: false, confort:false, horsgel:false, name: 'Entrée', id: '1', etat: ''},
+    {arret: false, eco: false, confort:false, horsgel:false, name: 'Emma', id: '2', etat: ''},
+    {arret: false, eco: false, confort:false, horsgel:false, name: 'Cuisine', id: '3', etat: ''},
+    {arret: false, eco: false, confort:false, horsgel:false, name: 'Milieu', id: '4', etat: ''},
+    {arret: false, eco: false, confort:false, horsgel:false, name: 'Salon', id: '5', etat: ''},
+    {arret: false, eco: false, confort:false, horsgel:false, name: 'SDB', id: '6', etat: ''},
+    {arret: false, eco: false, confort:false, horsgel:false, name: 'Douche', id: '7', etat: ''}
+  ];
+
   console.log("Récupération état de tous les chauffages");
   url = 'https://api.spark.io/v1/devices/'+device_id+'/etatfp?access_token='+token;
   request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       reponse = JSON.parse(body);
-      //console.log(reponse);
-      //console.log(reponse.result);
       var etats = reponse.result.split('');
-      console.log(etats);
       for (var i=0;i<7;i++) {
         if (etats[i] == "A") {
           chauffages[i].etat = etats[i];
           chauffages[i].arret = true;
           console.log(chauffages[i].name, chauffages[i].etat);
-        } else if (etats[i] = "E") {
+        } else if (etats[i] == "E") {
           chauffages[i].etat = etats[i];
           chauffages[i].eco = true;
           console.log(chauffages[i].name, chauffages[i].etat);
-        } else if (etats[i] = "H") {
+        } else if (etats[i] == "H") {
           chauffages[i].etat = etats[i];
           chauffages[i].horsgel = true;
           console.log(chauffages[i].name, chauffages[i].etat);
-        } else if (etats[C] = "C") {
+        } else if (etats[i] == "C") {
           chauffages[i].etat = etats[i];
           chauffages[i].confort = true;
           console.log(chauffages[i].name, chauffages[i].etat);
         }
       };
-      //console.log(etats[i])
       var data = {
-        chauffages: chauffages
+        chauffages: chauffages,
+        valeurOK : 'hidden',
+        valeurNOK : 'hidden'
       };
       res.render('index', data)
     };
   });
 });
 
-router.get('/simple', function(req, res){
-  var data = {name: 'Gorilla'};
-  res.render('simple', data);
-});
+//router.get('/simple', function(req, res){
+//  var data = {name: 'Gorilla'};
+//  res.render('simple', data);
+//});
+//
+//router.get('/logic', function(req, res){
+//  var data = {
+//    upIsUp: true,
+//    downIsUp: false,
+//    skyIsBlue: "yes"
+//  };
+//  
+//  res.render('logic', data);
+//});
 
-router.get('/logic', function(req, res){
-  var data = {
-    upIsUp: true,
-    downIsUp: false,
-    skyIsBlue: "yes"
-  };
-  
-  res.render('logic', data);
-});
+router.post("/",function(req,res){
+  var chauffages = [
+    {arret: false, eco: false, confort:false, horsgel:false, name: 'Entrée', id: '1', etat: ''},
+    {arret: false, eco: false, confort:false, horsgel:false, name: 'Emma', id: '2', etat: ''},
+    {arret: false, eco: false, confort:false, horsgel:false, name: 'Cuisine', id: '3', etat: ''},
+    {arret: false, eco: false, confort:false, horsgel:false, name: 'Milieu', id: '4', etat: ''},
+    {arret: false, eco: false, confort:false, horsgel:false, name: 'Salon', id: '5', etat: ''},
+    {arret: false, eco: false, confort:false, horsgel:false, name: 'SDB', id: '6', etat: ''},
+    {arret: false, eco: false, confort:false, horsgel:false, name: 'Douche', id: '7', etat: ''}
+  ];
 
-router.post("/chauffage",function(req,res){
   console.log("Changement état de tous les chauffages");
-  console.log(req.body.params);
- 
+  params = '';
+  for(var i in req.body)
+    params += req.body[i]
+  console.log(params);
+  
   request({
     url: 'https://api.spark.io/v1/devices/'+device_id+'/fp?access_token='+token,
     //qs: {from: 'blog example', time: +new Date()}, //Query string data
@@ -133,14 +145,42 @@ router.post("/chauffage",function(req,res){
     //Lets post the following key/values as form
     json: {
       //access_token: token,
-      params: req.body.params
+      params: params
     }}, function(error, response, body){
-      if(error) {
+      if(response.statusCode != 200) {
         console.log(error);
+        var data = {
+          //chauffages: chauffages,
+          valeurOK : 'hidden',
+          valeurNOK : '',
+        };
       } else {
-        console.log(response.statusCode, body);
-        res.sendStatus(response.statusCode)
-      }
+        for (var i=0;i<7;i++) {
+          if (params[i] == "A") {
+            chauffages[i].etat = params[i];
+            chauffages[i].arret = true;
+            console.log(chauffages[i].name, chauffages[i].etat);
+          } else if (params[i] == "E") {
+            chauffages[i].etat = params[i];
+            chauffages[i].eco = true;
+            console.log(chauffages[i].name, chauffages[i].etat);
+          } else if (params[i] == "H") {
+            chauffages[i].etat = params[i];
+            chauffages[i].horsgel = true;
+            console.log(chauffages[i].name, chauffages[i].etat);
+          } else if (params[i] == "C") {
+            chauffages[i].etat = params[i];
+            chauffages[i].confort = true;
+            console.log(chauffages[i].name, chauffages[i].etat);
+          }
+        };
+        var data = {
+          chauffages: chauffages,
+          valeurOK : '',
+          valeurNOK : 'hidden',
+        };
+       }
+      res.render('index', data)
     });
 });
 
